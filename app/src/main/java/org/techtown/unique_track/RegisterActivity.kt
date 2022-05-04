f95da46d9ce70d90fc478ea7289faa95af736f46
kafuowi <pressy286@gmail.com>
2022-05-04 19:39:37 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/unique_track/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.unique_track
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
