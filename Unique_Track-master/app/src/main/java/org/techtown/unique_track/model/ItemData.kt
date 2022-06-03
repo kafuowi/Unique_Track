91d35bc4e37cd8480bf4fea67b18686b6d41ce93
hyeonbinjin <jhb6323@naver.com>
2022-06-03 13:18:59 +0900

    delete overlap folder

--- Unique_Track-master/app/src/main/java/org/techtown/unique_track/model/ItemData.kt
+++
@@ -1,9 +0,0 @@
-package org.techtown.unique_track.model
-
-/* recyclerview 데이터 형식 - realtime database의 키와 변수의 이름이 같아야함 */
-data class ItemData(
-    val productName:String ?= null,
-    val registerDate:String ?= null,
-    val image:String ?= null,
-    val ownerUID:String ?= null
-)
